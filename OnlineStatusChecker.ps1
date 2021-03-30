97f37761a271e2f9a47c0d9e7e90e18f11bb3c29
ithelpab <81639316+ithelpab@users.noreply.github.com>
2021-03-30 12:07:05 -0400

    Create OnlineStatusChecker.ps1

---
+++ OnlineStatusChecker.ps1
@@ -0,0 +1,23 @@
+﻿function OnlineStatusCheck
+{
+Get-Date
+try
+{
+invoke-webrequest www.msn.com | select-object StatusDescription -ErrorAction Stop | Out-Null
+Write-Host "Online" -ForegroundColor Green
+} catch {
+        Write-Host "Offline" -ForegroundColor Red
+        [console]::beep(2620,700)
+}
+Write-Output -InputObject ----------------------------------
+Start-Sleep -seconds 15
+}
+
+
+Start-transcript -Path C:\logs.txt
+
+Do 
+{
+    OnlineStatusCheck
+}
+While (1)
