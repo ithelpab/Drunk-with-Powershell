4c96fd34d57bb226062b6140e428fe6a5ab55fda
ithelpab <81639316+ithelpab@users.noreply.github.com>
2021-03-30 13:05:38 -0400

    Create ComputerNameElseIf.ps1

---
+++ ComputerNameElseIf.ps1
@@ -0,0 +1,11 @@
+﻿if ($env:computerName.StartsWith("ES"))
+{
+msiexec.exe /i "C:\CDT_temp\ESdrc_insight_setup_10.2.0-23.msi" ALLUSERS=1 /qn /norestart /log output.log
+}
+elseif ($env:computerName.StartsWith("HS"))
+{
+msiexec.exe /i "C:\CDT_temp\HSdrc_insight_setup_10.2.0-23.msi" ALLUSERS=1 /qn /norestart /log output.log
+}
+else
+{
+}
