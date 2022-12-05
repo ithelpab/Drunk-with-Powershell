9dfde427ae4609078632ef651fd52b64e6945339
ithelpab <81639316+ithelpab@users.noreply.github.com>
2022-12-05 11:21:41 -0500

    Create DhcpThreshold.ps1

---
+++ DhcpThreshold.ps1
@@ -0,0 +1,6 @@
+﻿#DHCP server Name
+$_DHCPServer = 'morgan'
+$_PercentageThreshhold = 50
+
+
+Get-DhcpServerv4ScopeStatistics -ComputerName $_DHCPServer | Where-Object -FilterScript { $_.PercentageInUse -Gt $_PercentageThreshhold }
